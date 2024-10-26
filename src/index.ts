7cebe7d98501f1a42d431a8cf744c37bb166d9be
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-27 01:31:10 +0300

    Create cards variable

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+var cards = [{ shape: 'hexagon', color: 'red' }, { shape: 'triangle', color: 'blue' }, { shape: 'square', color: 'pink' }, { shape: 'x', color:  'black' },
+{ shape: 'hexagon', color: 'red' }, { shape: 'triangle', color: 'blue' }, { shape: 'square', color: 'pink' }, { shape: 'x', color:  'black' },
+{ shape: 'hexagon', color: 'black' }, { shape: 'triangle', color: 'pink' }, { shape: 'square', color: 'blue' }, { shape: 'x', color:  'red' },
+{ shape: 'hexagon', color: 'black' }, { shape: 'triangle', color: 'pink' }, { shape: 'square', color: 'blue' }, { shape: 'x', color:  'red' }]
+
+function onInit() {
+    
+}
