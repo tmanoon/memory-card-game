c66f0061841c1c40fb6e815b57d45c7a5cb2f6b4
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-10-27 10:43:52 +0200

    Edit functionality of on init function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cards = [];
+function onInit() {
+    const cardsEl = document.querySelector('.cards');
+    let htmlStr = '';
+    initCards();
+    for (let i = 0; i < cards.length; i++) {
+        const currCard = cards[i];
+        const cardEl = `<div class="card ${currCard.color} flex center"><p>${currCard.icon}</p></div>`;
+        htmlStr += cardEl;
+    }
+    cardsEl.innerHTML += htmlStr;
+}
+function initCards() {
+    cards = [{ shape: 'hexagon', icon: '⎔', color: 'red' }, { shape: 'triangle', icon: '△', color: 'blue' }, { shape: 'square', icon: '☐', color: 'pink' }, { shape: 'x', icon: '✕', color: 'black' },
+        { shape: 'hexagon', icon: '⎔', color: 'red' }, { shape: 'triangle', icon: '△', color: 'blue' }, { shape: 'square', icon: '☐', color: 'pink' }, { shape: 'x', icon: '✕', color: 'black' },
+        { shape: 'hexagon', icon: '⎔', color: 'black' }, { shape: 'triangle', icon: '△', color: 'pink' }, { shape: 'square', icon: '☐', color: 'blue' }, { shape: 'x', icon: '✕', color: 'red' },
+        { shape: 'hexagon', icon: '⎔', color: 'black' }, { shape: 'triangle', icon: '△', color: 'pink' }, { shape: 'square', icon: '☐', color: 'blue' }, { shape: 'x', icon: '✕', color: 'red' }];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-var cards = [{ shape: 'hexagon', color: 'red' }, { shape: 'triangle', color: 'blue' }, { shape: 'square', color: 'pink' }, { shape: 'x', color:  'black' },
-{ shape: 'hexagon', color: 'red' }, { shape: 'triangle', color: 'blue' }, { shape: 'square', color: 'pink' }, { shape: 'x', color:  'black' },
-{ shape: 'hexagon', color: 'black' }, { shape: 'triangle', color: 'pink' }, { shape: 'square', color: 'blue' }, { shape: 'x', color:  'red' },
-{ shape: 'hexagon', color: 'black' }, { shape: 'triangle', color: 'pink' }, { shape: 'square', color: 'blue' }, { shape: 'x', color:  'red' }]
+import { Card } from "./models/card.model"
+
+var cards: Card[] = []
 
 function onInit() {
-    
+    const cardsEl = document.querySelector('.cards') as HTMLElement
+    let htmlStr = ''
+    initCards()
+    for (let i = 0; i < cards.length; i++) {
+        const currCard = cards[i]
+        const cardEl = `<div class="card ${currCard.color} flex center"><p>${currCard.icon}</p></div>`
+        htmlStr += cardEl
+    }
+    cardsEl.innerHTML += htmlStr
+}
+
+function initCards() {
+    cards = [{ shape: 'hexagon', icon: '⎔', color: 'red' }, { shape: 'triangle', icon: '△', color: 'blue' }, { shape: 'square', icon: '☐', color: 'pink' }, { shape: 'x', icon: '✕', color: 'black' },
+    { shape: 'hexagon', icon: '⎔', color: 'red' }, { shape: 'triangle', icon: '△', color: 'blue' }, { shape: 'square', icon: '☐', color: 'pink' }, { shape: 'x', icon: '✕', color: 'black' },
+    { shape: 'hexagon', icon: '⎔', color: 'black' }, { shape: 'triangle', icon: '△', color: 'pink' }, { shape: 'square', icon: '☐', color: 'blue' }, { shape: 'x', icon: '✕', color: 'red' },
+    { shape: 'hexagon', icon: '⎔', color: 'black' }, { shape: 'triangle', icon: '△', color: 'pink' }, { shape: 'square', icon: '☐', color: 'blue' }, { shape: 'x', icon: '✕', color: 'red' }]
 }
